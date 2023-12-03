639270503af71fa7f337ff72a38ebad66e09a477
Nathan Alderson <me@nathanalderson.com>
2023-12-03 14:00:58 -0600

    Day 3 - optimized as graph

--- day03/solution.py
+++ day03/solution.py
@@ -2,6 +2,19 @@ from pprint import pprint
 from dataclasses import dataclass
 import re
 
+TEST_INPUT = """\
+467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..
+"""
+
 
 @dataclass(frozen=True)
 class Point:
@@ -18,10 +31,17 @@ class Point:
         yield Point(self.x - 1, self.y + 1)
         yield Point(self.x - 1, self.y - 1)
 
+    def __repr__(self):
+        return f"({self.x}, {self.y})"
+
 
 @dataclass(frozen=True)
 class Thing:
     points: list[Point]
+
+    @property
+    def first_point(self):
+        return self.points[0]
 
     def adjacent_points(self):
         return set(flatten(p.adjacent_points() for p in self.points)) - set(self.points)
@@ -31,10 +51,82 @@ class Thing:
 class Symbol(Thing):
     char: str
 
+    def __repr__(self):
+        return f"{self.char}@{self.first_point}"
+
 
 @dataclass(frozen=True)
 class Number(Thing):
     value: int
+
+    def __repr__(self):
+        return f"{self.value}@{self.first_point}"
+
+
+@dataclass
+class Node:
+    thing: Thing
+    edges: list["Edge"]
+
+    def __repr__(self):
+        return f"Node< {self.thing} >"
+
+    def connects_to(self):
+        return [e.other_node(self) for e in self.edges]
+
+    def is_partnumber(self):
+        return isinstance(self.thing, Number) and any(
+            isinstance(n.thing, Symbol) for n in self.connects_to()
+        )
+
+    def gear_ratio(self):
+        if isinstance(self.thing, Symbol) and self.thing.char == "*":
+            if len(self.edges) == 2:
+                return mult(e.other_node(self).thing.value for e in self.edges)
+        return 0
+
+
+def mult(iterable):
+    result = 1
+    for i in iterable:
+        result *= i
+    return result
+
+
+@dataclass
+class Edge:
+    node1: Node
+    node2: Node
+
+    def __repr__(self):
+        return f"Edge<{self.node1} <-> {self.node2}>"
+
+    def other_node(self, node):
+        if node == self.node1:
+            return self.node2
+        elif node == self.node2:
+            return self.node1
+        else:
+            raise ValueError(f"Node {node} not in edge {self}")
+
+
+@dataclass
+class Graph:
+    nodes: dict[Point, Node]
+
+    def get_or_add(self, thing: Thing):
+        if node := self.nodes.get(thing.first_point):
+            return node
+        node = Node(thing, [])
+        self.nodes[thing.first_point] = node
+        return node
+
+    def connect(self, thing1: Thing, thing2: Thing):
+        node1 = self.get_or_add(thing1)
+        node2 = self.get_or_add(thing2)
+        edge = Edge(node1, node2)
+        node1.edges.append(edge)
+        node2.edges.append(edge)
 
 
 def parse_symbols(lines):
@@ -57,38 +149,36 @@ def flatten(ll):
     return [i for l in ll for i in l]
 
 
-def is_partnumber(number: Number, symbol_locs: set[Point]):
-    return any(p in symbol_locs for p in number.adjacent_points())
+def part1(graph: Graph):
+    part_numbers = [n for n in graph.nodes.values() if n.is_partnumber()]
+    return sum(n.thing.value for n in part_numbers)
 
 
-def gear_ratio(symbol: Symbol, numbers: list[Number]):
-    if symbol.char == "*":
-        adjacent_numbers = [
-            n for n in numbers if any(p in symbol.adjacent_points() for p in n.points)
-        ]
-        if len(adjacent_numbers) == 2:
-            return adjacent_numbers[0].value * adjacent_numbers[1].value
-    return 0
+def part2(graph):
+    return sum(n.gear_ratio() for n in graph.nodes.values())
 
 
-def part1(numbers: list[Number], symbols: list[Symbol]):
-    symbol_locs = set(flatten(s.points for s in symbols))
-    part_numbers = [n for n in numbers if is_partnumber(n, symbol_locs)]
-    return part_numbers, sum(n.value for n in part_numbers)
-
-
-def part2(numbers: list[Number], symbols: list[Symbol]):
-    return sum(gear_ratio(s, numbers) for s in symbols)
+def build_graph(numbers, symbols):
+    graph = Graph({})
+    symbols_by_location = {p: s for s in symbols for p in s.points}
+    for n in numbers:
+        for p in n.adjacent_points():
+            if s := symbols_by_location.get(p):
+                graph.connect(n, s)
+    return graph
 
 
 def main():
     with open("input") as f:
         lines = f.readlines()
+    # lines = TEST_INPUT.splitlines()
     symbols = list(parse_symbols(lines))
     numbers = list(parse_numbers(lines))
-    part_numbers, ans1 = part1(numbers, symbols)
-    print(ans1)
-    print(part2(part_numbers, symbols))
+    graph = build_graph(numbers, symbols)
+    pprint(graph)
+    print(part1(graph))
+    print(part2(graph))
+    # answers: 550934, 81997870
 
 
 if __name__ == "__main__":
