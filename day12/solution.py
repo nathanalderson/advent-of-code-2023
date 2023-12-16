4f6ccec9eb6be255b06102f6f760adf50dfe3ef4
Nathan Alderson <me@nathanalderson.com>
2023-12-15 22:19:13 -0600

    Day 12 - part 2 rework WIP

--- day12/solution.py
+++ day12/solution.py
@@ -1,5 +1,6 @@
 from enum import Enum
 import itertools
+from pprint import pprint
 from typing import Iterable
 
 
@@ -39,6 +40,9 @@ class State(Enum):
             case State.DAMAGED:
                 return "#"
 
+    def __repr__(self) -> str:
+        return str(self)
+
 
 class Record:
     def __init__(self, line, unfold=False) -> None:
@@ -59,26 +63,34 @@ class Record:
         states = "".join(str(s) for s in self.states)
         return f"<{states}>{self.checksum}"
 
-    def solve(self) -> Iterable[Checksum]:
-        num_unknown = self.states.count(State.UNKNOWN)
-        candidates = itertools.product(
-            [State.WORKING, State.DAMAGED], repeat=num_unknown
-        )
-        for candidate in candidates:
-            candidate = list(candidate)
-            states = self.states.copy()
-            for i, c in enumerate(states):
-                if c == State.UNKNOWN:
-                    states[i] = candidate.pop()
-            if checksum(states) == self.checksum:
-                yield states
+    def check(states: list[State], checksum: Checksum) -> bool:
+        return checksum == checksum(states)
+
+    def solve(self, states: tuple[State], checksum: tuple[int], acc: int) -> int:
+        if len(states) == 0 and len(checksum) == 0:
+            return acc + 1
+        elif len(states) == 0 or len(checksum) == 0:
+            return acc
+        elif len(states) < checksum[0]:
+            return acc
+        elif any(s == State.WORKING for s in states[: checksum[0]]):
+            return acc
+        else:
+            head, *rest = states
+            if head == State.WORKING:
+                return self.solve(rest, checksum, acc)
+            if head == State.UNKNOWN:
+                return self.solve((State.WORKING, *rest), checksum, acc) + self.solve(
+                    (State.DAMAGED, *rest), checksum, acc
+                )
+            else:  # state == State.DAMAGED
+                return self.solve(states[: checksum[0]], checksum[1:], acc)
 
     def num_solutions(self) -> int:
-        return sum(1 for _ in self.solve())
+        return self.solve(tuple(self.states), tuple(self.checksum), 0)
 
 
 def checksum(states: list[State]) -> list[int]:
-    assert not any(s == State.UNKNOWN for s in states)
     damaged_groups = (
         list(g) for k, g in itertools.groupby(states) if k == State.DAMAGED
     )
@@ -87,13 +99,15 @@ def checksum(states: list[State]) -> list[int]:
 
 def main():
     lines = TEST_INPUT.splitlines()
-    # lines = open("input").readlines()
+    lines = open("input").readlines()
     records1 = [Record(line) for line in lines]
     ans1 = sum(r.num_solutions() for r in records1)
     print(ans1)
-    records2 = [Record(line, unfold=True) for line in lines]
-    ans2 = sum(r.num_solutions() for r in records2)
-    print(ans2)
+    # records2 = [Record(line, unfold=True) for line in lines]
+    # r = records2[1]
+    # print(r.num_solutions())
+    # ans2 = sum(r.num_solutions() for r in records2)
+    # print(ans2)
 
 
 if __name__ == "__main__":
